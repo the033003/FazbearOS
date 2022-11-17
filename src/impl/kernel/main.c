2205fed48f44d7ea4ffd236d260e9dfd90a6a53b
the033003 <the033003@gmail.com>
2022-11-16 17:47:35 -0700

    Update main.c

--- src/impl/kernel/main.c
+++ src/impl/kernel/main.c
@@ -3,5 +3,5 @@
 void kernel_main() {
     print_clear();
     print_set_color(PRINT_COLOR_WHITE, PRINT_COLOR_BLACK);
-    print_str("Welcome to Fazbear OS\n\nCongrats on booting properly!");
+    print_str("Welcome to Fazbear OS!\n\nThis is the whole OS :)");
 }
