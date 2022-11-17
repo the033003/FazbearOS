144d9985fa5520fb472a836b3ae83e45b3670dd6
the033003 <the033003@gmail.com>
2022-11-17 13:23:24 -0700

    Fix main.c

--- src/impl/kernel/main.c
+++ src/impl/kernel/main.c
@@ -1,9 +1,9 @@
 #include "print.h"
 
-void main() {
+void kernel_main() {
 
     print_clear();
     print_set_color(PRINT_COLOR_WHITE, PRINT_COLOR_BLACK);
-    print_str("Welcome to Fazbear OS!");
+    print_str("Welcome to Fazbear OS!\n\nCongrats on Booting properly!");
 
 }
